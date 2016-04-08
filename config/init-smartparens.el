f3a0c9ca08a9a576dc524d726c35050c03bc9f6b
unknown <keke.xiang@PA1407001.corp.jingle.cn>
2016-04-08 15:02:38 +0800

    Disable auto-complete and smartparens in lower version

--- config/init-smartparens.el
+++ config/init-smartparens.el
@@ -1,7 +1,8 @@
-(require-package 'smartparens)
-
-(require 'smartparens-config)
-(smartparens-global-mode 1)
-(show-smartparens-global-mode +1)
+(unless (version<= emacs-version "24")
+  (progn
+    (require-package 'smartparens)
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-smartparens-global-mode +1)))
 
 (provide 'init-smartparens)
