150fb85d36d176aecac2a94e9eca7cbb33b5cb34
Keke Xiang <keke.xiang@garena.cn>
2016-07-08 15:31:15 +0800

    fix docker in lower version

--- config/init-docker.el
+++ config/init-docker.el
@@ -1,5 +1,6 @@
 (unless (version<= emacs-version "24.4")
-  (require-package 'dockerfile-mode))
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+  (progn
+    (require-package 'dockerfile-mode)
+    (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))))
 
 (provide 'init-docker)
